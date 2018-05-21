74c8fec964a83f2daeca3dcaab6215885c3b0fd1
Pendramon <pendramon2@gmail.com>
2018-05-22 01:56:53 +0200

    Add project files.

---
+++ ToshinouBot/Controllers/MainHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using System.Linq;
+
+namespace ToshinouBot.Controllers
+{
+    class MainHandler
+    {
+        public DiscordSocketClient Client;
+
+        public MainHandler(DiscordSocketClient Discord) {
+            Discord.Ready += OnReadyUpdateBrahDuh;
+            Discord.GuildMemberUpdated += OnBrahDuhUpdated;
+            Client = Discord;
+        }
+
+        private async Task OnReadyUpdateBrahDuh() {
+            SocketGuild guild = Client.GetGuild(434604419255107586);
+            SocketRole role = guild.GetRole(435902767358410762);
+            SocketGuildUser user = guild.GetUser(427905551763111950);
+
+            if (!user.Roles.Contains(role)) {
+                Console.WriteLine("Updated BruhDuh");
+                await user.AddRoleAsync(role);
+            }
+        }
+
+        private async Task OnBrahDuhUpdated(SocketGuildUser before, SocketGuildUser after)
+        {
+            if (after.Id == 427905551763111950)
+            {
+                SocketGuild guild = Client.GetGuild(434604419255107586);
+                SocketRole role = guild.GetRole(435902767358410762);
+
+                if (!after.Roles.Contains(role))
+                {
+                    Console.WriteLine("Updated BruhDuh");
+                    await after.AddRoleAsync(role);
+                }
+            }
+        }
+    }
+}
---
+++ ToshinouBot/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using ToshinouBot.Services;
+using System.Linq;
+using System.Collections.Generic;
+using ToshinouBot.Controllers;
+
+namespace ToshinouBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private IConfiguration _config;
+
+        private MainHandler MainHandler;
+
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                AlwaysDownloadUsers = true
+            });
+
+            _config = BuildConfig();
+
+            var services = ConfigureServices();
+            services.GetRequiredService<LogService>();
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync(services);
+
+            MainHandler = new MainHandler(_client);
+            await _client.SetGameAsync(null);
+
+            await _client.LoginAsync(TokenType.Bot, _config["token"]);
+            await _client.StartAsync();
+
+
+            
+            await Task.Delay(-1);
+
+           
+        }
+
+
+        //SocketGuildUser user = guild.GetUser(427905551763111950);
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                // Base
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                // Logging
+                .AddLogging()
+                .AddSingleton<LogService>()
+                // Extra
+                .AddSingleton(_config)
+                // Add additional services here...
+                .BuildServiceProvider();
+        }
+
+        private IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
+        }
+    }
+}
